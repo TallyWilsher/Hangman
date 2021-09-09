81734486ecbf5076fb3e9a601959c11027f677ab
Tally Wilsher <TWilsher@1800contacts.com>
2021-09-09 08:18:08 -0600

    add components and services

--- hangman-app/src/app/app.module.ts
+++ hangman-app/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HangmanGameComponent } from './hangman-game/hangman-game.component';
+import { PlayerComponent } from './player/player.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HangmanGameComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
---
+++ hangman-app/src/app/hangman-game/hangman-game.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hangman-game',
+  templateUrl: './hangman-game.component.html',
+  styleUrls: ['./hangman-game.component.css']
+})
+export class HangmanGameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
